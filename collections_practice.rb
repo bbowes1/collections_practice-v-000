51fbaf824d930a86c0cbead95d1125e91e2a0dcc
Benjamin Bowes <bbowes999@gmail.com>
2018-03-27 11:45:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(array)
+array.sort
+end
+
+def sort_array_desc(array)
+array.sort! {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+array.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+ array = [1,2,3]
+array.sort! {|1, 2| 2 <=> 1}
+end
 
